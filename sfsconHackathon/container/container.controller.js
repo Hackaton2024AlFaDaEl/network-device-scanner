e74c44db8341b6650b40ced0405c42d2177008a5
Fabian925 <fabian.roalter@gmail.com>
2024-11-09 13:31:21 +0100

    NSD - Klammer

--- sfsconHackathon/container/container.controller.js
+++ sfsconHackathon/container/container.controller.js
@@ -4,8 +4,8 @@ const model = require("./container.model");
 
 function startNetwork(req, res) {
     model.scanNetwork()
-        .then(next => res.send(view.renderView(next))
-        .catch(next => res.redirect("/")));
+        .then(next => res.send(view.renderView(next)))
+        .catch(next => res.redirect("/"));
 }
 
 module.exports = { startNetwork };
