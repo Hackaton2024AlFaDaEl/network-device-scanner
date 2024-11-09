f9afbf71cc8ec2b7dd64adf2888d3d5270d3023b
Fabian925 <fabian.roalter@gmail.com>
2024-11-09 04:49:55 +0100

    NDS - Code Review - Keine Crashes mehr. - Quality of life changes.

--- sfsconHackathon/container/container.model.js
+++ sfsconHackathon/container/container.model.js
@@ -2,72 +2,106 @@ const ping = require('ping');
 const arp = require('node-arp');
 const os = require('os');
 const https = require('https');
-const { Subject, firstValueFrom } = require('rxjs');
+const { last, Observable } = require('rxjs');
 
 function getLocalNetworkRange() {
     const interfaces = os.networkInterfaces();
-    for (let iface in interfaces) {
-        for (let i of interfaces[iface]) {
+    for (const iface in interfaces) {
+        for (const i of interfaces[iface]) {
             if (i.family === 'IPv4' && !i.internal) {
-                const parts = i.address.split('.');
-                parts[3] = '0'; // Set the base IP address to start scanning
-                return parts.join('.');
+                return i;
             }
         }
     }
     return null;
 }
 
-const networkBase = getLocalNetworkRange();
-if (!networkBase) {
-    console.error('Unable to detect local network range.');
-    process.exit(1);
+function calcNetworkAddress(ip, netmask) {
+    const ipParts = ip.split('.').map(Number);
+    const netmaskParts = netmask.split('.').map(Number);
+    return ipParts.map((part, index) => part & netmaskParts[index]).join('.');
 }
+
+function calcBroadcastAddress(ip, subnetMask) {
+    const ipParts = ip.split('.').map(Number);
+    const subnetMaskParts = subnetMask.split('.').map(Number);
+
+    // Calculate inverted subnet mask
+    const invertedSubnetMaskParts = subnetMaskParts.map(part => ~part & 255);
+
+    // Calculate broadcast address by applying OR between IP and inverted subnet mask
+    const broadcastAddressParts = ipParts.map((part, index) => part | invertedSubnetMaskParts[index]);
+
+    // Convert back to a dotted decimal string
+    return broadcastAddressParts.join('.');
+}
+
+let count = 0;
 
 async function getManufacture(mac) {
     const apiKey = "01jc76e8szh5jd4bj537a845dm01jc76fm3y6rjeawasj2yj0af6z7p3z9omfsdb";
-    const url = "https://api.maclookup.app/v2/macs"
-    let sub = new Subject();
-    let response = "";
-    https.get(`${url}/${mac}?apiKey=${apiKey}`, res => {
-        res.on("data", res => response += res)
-        res.on("end", () => {
-            response = JSON.parse(response);
-            response = response.company;
-            sub.next(null)
-        })
-    }).on("error", err => console.error(err));
-    await firstValueFrom(sub.asObservable());
-    return response;
+    const url = `https://api.maclookup.app/v2/macs/${mac}?apiKey=${apiKey}`;
+    count++;
+    if (count > 50) {
+        await new Promise(r => setTimeout(r, 1000));
+        count = 0;
+    }
+
+    return new Promise((resolve, reject) => {
+        https.get(url, res => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => {
+                try {
+                    const response = JSON.parse(data);
+                    resolve(response.company || 'Unknown');
+                } catch (error) {
+                    reject('Parse Error');
+                }
+            });
+        }).on('error', d => console.error("do isch a fehelr"));
+    });
+}
+
+function ipAddrAddOne(ip) {
+    const segments = ip.split('.').map(Number);
+    for (let i = segments.length - 1; i >= 0; i--) {
+        if (segments[i] < 255) {
+            segments[i]++;
+            break;
+        } else {
+            segments[i] = 0;
+        }
+    }
+    return segments.join('.');
 }
 
 async function scanNetwork() {
-    const subnet = networkBase.slice(0, networkBase.lastIndexOf('.') + 1);
-    const promises = [];
-
-    for (let i = 1; i < 255; i++) {
-        const ip = `${subnet}${i}`;
-        promises.push(
-            new Promise((resolve) => {
-                ping.promise.probe(ip, { timeout: 1 })
-                    .then(res => res.alive 
-                        ? arp.getMAC(ip, async (err, mac) => 
-                            resolve({ 
-                            ip, 
-                            mac: mac || 'N/A', 
-                            manufacture: mac ? 
-                                await getManufacture(mac) : 
-                                "N/A"}))
-                        :resolve(null)
-                    );
-            })
-        );
+    const networkBase = getLocalNetworkRange();
+    if (!networkBase) return [];
+    
+    const networkStart = calcNetworkAddress(networkBase.address, networkBase.netmask);
+    const broadcast = calcBroadcastAddress(networkBase.address, networkBase.netmask);
+    
+    let currentIp = networkStart;
+    while (currentIp !== broadcast) {
+        promises.push(new Promise((resolve) => {
+            ping.promise.probe(currentIp, { timeout: 1 }).then(async (res) => {
+                if (res.alive) {
+                    arp.getMAC(res.inputHost, async (err, mac) => {
+                        const manufacture = mac ? await getManufacture(mac) : "N/A";
+                        resolve({ ip: res.inputHost, mac: mac || 'N/A', manufacture });
+                    });
+                } else {
+                    resolve(null);
+                }
+            }).catch(() => resolve(null));
+        }));
+        currentIp = ipAddrAddOne(currentIp);
     }
 
-    let networks = await Promise.all(promises);
-    network = networks.filter(net => net != null)
-
-    return network;
+    const networks = (await Promise.all(promises)).filter(net => net !== null);
+    return networks;
 }
 
 module.exports = { scanNetwork };
